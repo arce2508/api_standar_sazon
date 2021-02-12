d8c62a3961be3ca7197cdad67471a6886a49a004
Shari <silver_899@hotmail.com>
2021-02-11 23:43:05 -0600

    update env file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const server = require('./src/server')
 
 server.listen('8080', () => {
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
 
-const url = 'mongodb+srv://eric:eric123@decima-test.nnrg3.mongodb.net/standar+sazon'
+const url = process.env.DB_URL
 
 const conect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
