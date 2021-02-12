f37dc92ab85917b1a74970e420c565599b82a984
Eric Trujillo <eric.trujillo.art@gmail.com>
2021-02-12 16:09:18 -0600

    [CREATION] Main Recipe model

---
+++ src/models/recipes.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const recipeSchema = new mongoose.Schema({
+    recipeName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        enum: ['starters', 'soups', 'main courses', 'desserts', 'sauces', 'drinks'],
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    },
+    image: {
+        type: String,
+        trim: true
+    },
+    performancePercent: {
+        type: Number,
+        require: true,
+        trim: true
+    },
+    technique: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    ingredients: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Products'
+    }],
+    SubRecipes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubRecipe'
+    }],
+    timestamp: true
+})
+
+module.exports = mongoose.model('MainRecipe', recipeSchema)
