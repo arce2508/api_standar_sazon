503b9ca29caeb4fe73baf51aceab854a238c4a16
Eric Trujillo <eric.trujillo.art@gmail.com>
2021-02-13 00:55:36 -0600

    [Upgrade]Recipe structure

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -1,7 +1,77 @@
 const mongoose = require('mongoose')
+const ingredientSchema = require('./ingredients')
+
+const timePerformanceSchema = new mongoose.Schema({
+    production:{
+        type: Number,
+        required: true
+    },
+    performance: {
+        type: Number,
+        required: true 
+    },
+    portionSize: {
+        type: Number,
+        required: true
+    },
+    portionQuantity: {
+        type: Number,
+        required: true
+    },
+    preparationTime: {
+        type: Number,
+        required: true
+    },
+    cookingTime: {
+        type: Number,
+        required: true
+    },
+    operatingTemperature:{
+        type: Number,
+        required: true
+    }
+})
+
+const directIndirectCostsSchema = new mongoose.Schema({
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    workForceDirect: {
+        type: Number,
+        required: true
+    },
+    priceSaleByPortion: {
+        type: Number,
+        required: true
+    },
+    percentProductionByPortion: {
+        type: Number,
+        required: true
+    },
+    productionIndirect: {
+        type: Number,
+        required: true
+    },
+    percentProductionByPortion: {
+        type: Number,
+        default: 0.70
+    },
+    utilityPercentByPortion: {
+        type: Number,
+        default: 0.30
+    },
+    portionUtility: {
+        type: Number
+    },
+    salesPriceByPortion: {
+        type: Number
+    }
+
+})
 
 const recipeSchema = new mongoose.Schema({
-    Name: {
+    name: {
         type: String,
         required: true,
         trim: true
@@ -15,32 +85,36 @@ const recipeSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Users'
     },
-    image: {
+    presentation: {
         type: String,
         trim: true
     },
-    performancePercent: {
-        type: Number,
-        require: true,
-        trim: true
-    },
-    technique: {
+    productionWardrobe: [{
         type: String,
         required: true,
         trim: true
-    },
-    ingredients: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Products'
     }],
+    performancePercent: {
+        type: Number
+    },
+    technique: [{
+        type: String,
+        required: true,
+        trim: true
+    }],
+    ingredients: [
+        ingredientSchema
+    ],
     SubRecipes: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'SubRecipes'
     }],
-   
+    directIndirectCosts: directIndirectCostsSchema,
+    timePerformance: timePerformanceSchema
+
 },{
     timestamp: true
-} )
+})
 
 
 module.exports = mongoose.model('Recipes', recipeSchema)
