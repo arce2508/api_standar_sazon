405e4c70a8987afd0ffab4ed064b255c28b3ca58
arce <arcegutierrez.2508@gmail.com>
2021-02-26 01:05:33 -0600

    corrección de sintaxis en carpeta usecases/aut

--- src/usecases/auth.js
+++ src/usecases/auth.js
@@ -1,7 +1,7 @@
 
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const Users = require('../usecases/users')
+const Users = require('../models/users')
 
 async function login (email, password){
  
